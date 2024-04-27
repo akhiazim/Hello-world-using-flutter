1f7c282040c10f9cfc6e5fcf1f3ed8751bcf5bb8
akhiazim <143117905+akhiazim@users.noreply.github.com>
2024-04-27 11:54:38 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import "package:flutter/material.dart";
 
 void main() {
-  print("Hello World!");
+  print("Akhi Azim Papri");
 
   runApp(
-      MaterialApp(home: Scaffold(body: Center(child: Text("Hello World!")))));
+      MaterialApp(home: Scaffold(body: Center(child: Text("Akhi Azim Papri ")))));
 }
